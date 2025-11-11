f24abe2ba52bce9690bad4deadab6cdc126d2356
Edoc-ps <eeuardooctavio@gmail.com>
2025-11-11 18:56:28 -0300

    feat

---
+++ generate_docker.py
@@ -0,0 +1,35 @@
+import yaml
+
+services = {}
+isccp_ids = [f"{i:02d}" for i in range(1, 16)]
+
+for i in range(1, 16):
+    id_str = f"{i:02d}"
+    services[f"isccp_{id_str}"] = {
+        "build": "./gp_backend/SCCP",
+        "container_name": f"isccp_{id_str}",
+        "environment": [
+            f"ISCCP_ID={id_str}",
+            "BROKER=mqtt",
+        ],
+        "depends_on": ["mqtt"]
+    }
+
+
+for i in range(1, 25):
+    id_str = f"{i:02d}"
+    services[f"car_{id_str}"] = {
+        "build": "./gp_backend/cars",
+        "container_name": f"car_{id_str}",
+        "environment": [
+            f"CAR_ID={id_str}",
+            "MQTT_BROKER=mqtt"
+        ],
+        "depends_on": ["mqtt"] + [f"isccp_{id}" for id in isccp_ids]
+    }
+
+compose = {"services": services}
+
+# Salvar YAML
+with open("./docker_generated.yml", "w") as f:
+    yaml.dump(compose, f, default_flow_style=False, sort_keys=True)
--- gp_backend/cars/car_pub.py
+++ gp_backend/cars/car_pub.py
@@ -1,5 +1,6 @@
 
 import json
+import os
 import random
 from datetime import datetime
 import time
@@ -7,6 +8,7 @@ import paho.mqtt.client as mqtt
 
 broker = "mqtt"
 port = 1883
+id = os.getenv("CAR_ID")
 
 client = mqtt.Client()
 client.connect(broker, port, 60)
@@ -20,7 +22,7 @@ def gen_tire_data(compound="Macio"):
         "wear": random.randint(40, 70)
     }
 
-def gen_car(car_id="01", lap=1):
+def gen_car(car_id=id, lap=1):
     tire_data = {
         "frontLeft": gen_tire_data(),
         "frontRight": gen_tire_data(),
@@ -36,11 +38,11 @@ def gen_car(car_id="01", lap=1):
         "tireData": tire_data
     }
 
-def send_to_isccp(car_data):
+def send_to_isccp():
     isccp_list = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15"]
 
     for isccp in isccp_list:
-
+        car_data = gen_car() 
         car_data["sector"] = isccp 
         topic = f"/isccp/{isccp}/tires"
         payload = json.dumps(car_data)
@@ -48,10 +50,17 @@ def send_to_isccp(car_data):
         client.publish(topic, payload)
         print(f"[CAR] → Enviado para tópico {topic}")
 
-if __name__ == "__main__":
-    dados_carro = gen_car()
-    send_to_isccp(dados_carro)
-    print("simulação feita")
-    time.sleep(1)
+def main(): 
+    for lap in range(1, 72):
+        print(f"\n=== CARRO {id} — Lap {lap}/71 ===")
+        car_data = gen_car(id, lap)
+        send_to_isccp(car_data)
+
+        time.sleep(1)
+
+    print(f"\n✅ Carro {id} finalizou 71 voltas")
     client.disconnect()
+
+if __name__ == "__main__":
+    main()
   
