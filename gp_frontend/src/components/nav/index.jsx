11426956adb0554932f0086dda3a35c984687bd5
Eduardo Octávio <94386792+EdOc-PS@users.noreply.github.com>
2025-11-12 20:10:24 -0300

    Add files via upload

--- gp_frontend/src/components/nav/index.jsx
+++ gp_frontend/src/components/nav/index.jsx
@@ -1,20 +1,22 @@
 import { Link } from 'react-router-dom';
+import styles from "./nav.module.css";
 
 function Nav() {
     return (
         <>
-            <div style={{
-                width: "100%", height: 388, display: "flex", justifyContent: "end", backgroundColor: "#000",
-                borderBottom: "1px solid #303037", flexDirection: "column", alignItems: "center"
-            }}>
+            <nav className={styles.navContainer}>
+                <div className={styles.navImg}>       
                 <img src="https://media.formula1.com/image/upload/c_lfill,w_3392/q_auto/v1740000000/fom-website/manual/Misc/Red%20Box%20Image.webp" alt="" 
-                    style={{ width: "50%", height: 310, objectFit: "cover", marginBottom: "1rem", borderRadius: 10}}/>
+                    style={{ objectFit: "cover", borderRadius: 10}}/>
+                <span></span>
+                <h1>Monitor GP - São Paulo</h1>
+                </div>
                 <div style={{ width: "85%", padding: "0px 24px", backgroundColor: "#000", display: "flex", alignItems: "center" }}>
                     <p style={{ marginLeft: "26px" }} >2025</p>
                     <Link to='/cars' style={{ padding: "8px 24px" }}>Pilotos</Link>
                     <Link to='/dashboard' style={{ padding: "8px 24px" }}>Dashboard</Link>
                 </div>
-            </div>
+            </nav>
         </>
     )
 }
